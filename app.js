fec7548c8a519edb77d988d37877aeb80aa46f27
Sara <saravanessaguedesof@gmail.com>
2024-09-05 20:46:39 -0300

    inserindo javascript e base de dados

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log('Olá, mundo!')
+
+function pesquisar() {
+    let section = document.getElementById("resultados-pesquisa")
+
+    let resultados = "";
+
+    for (let dado of dados) {
+        resultados += `
+            <div class="item-resultado">
+                <h2>
+                    <a href="#" target="_blank">${dado.nome}</a>
+                </h2>
+                <p class="descricao-meta">${dado.descricao}</p>
+                <a href=${dado.link} target="_blank">Mais informações</a>
+            </div>
+        `;
+    }
+    section.innerHTML = resultados;
+}
+
+
+
---
+++ dados.js
@@ -0,0 +1,18 @@
+let dados = [
+    {
+        nome: "Java",
+        descricao: "Java é uma linguagem de programação orientada a objetos, robusta, segura e portável, amplamente utilizada no desenvolvimento de diversas aplicações, desde sistemas corporativos até aplicativos móveis e web.",
+        link: "https://pt.wikipedia.org/wiki/Java_(linguagem_de_programa%C3%A7%C3%A3o)"
+    },
+    {
+        nome: "Python",
+        descricao: "Python é uma linguagem de programação de alto nível, interpretada e de propósito geral. Conhecida por sua sintaxe clara e legível, Python é amplamente utilizada em diversas áreas, como ciência de dados, machine learning, desenvolvimento web e automação.",
+        link: "https://pt.wikipedia.org/wiki/Python"
+    },
+    {
+        nome: "JavaScrpt",
+        descricao: "JavaScript é uma linguagem de programação interpretada, cliente-side, utilizada principalmente para criar interatividade em páginas web. É uma linguagem essencial para o desenvolvimento front-end e também pode ser utilizada no back-end com frameworks como Node.js",
+        link:"https://pt.wikipedia.org/wiki/JavaScript"
+    }
+
+]
